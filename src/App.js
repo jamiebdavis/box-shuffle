0e001f14874ad27a2374995ecd874776667ec7f5
jamiebdavis <jamied1985@gmail.com>
2019-04-01 17:47:04 +0100

    reworked shuffle handler

--- src/App.js
+++ src/App.js
@@ -3,26 +3,32 @@ import "./App.css";
 
 class App extends Component {
   state = {
-    top: 0,
-    left: 0
+    redBox: {
+      top: 0,
+      left: 0
+    },
+    blueBox: {
+      top: 0,
+      left: 0
+    }
   };
 
   shuffle = () => {
     // TODO: using react state, shuffle .box's to
     // new positions within .box-container and update
     // labels
-    let topPosition = this.state.top;
-    topPosition = Math.random() * 300;
 
-    this.setState({ top: topPosition.toFixed() });
-    console.log("Top: " + this.state.top);
+    let redBox = this.state.redBox;
+    redBox.top = Math.floor(Math.random() * 300);
+    redBox.left = Math.floor(Math.random() * 300);
 
-    let leftPosition = this.state.left;
+    this.setState({ redBox });
 
-    leftPosition = Math.random() * 300;
+    let blueBox = this.state.blueBox;
+    blueBox.top = Math.floor(Math.random() * 300);
+    blueBox.left = Math.floor(Math.random() * 300);
 
-    this.setState({ left: leftPosition.toFixed() });
-    console.log("Left: " + this.state.left);
+    this.setState({ blueBox });
   };
 
   render() {
@@ -30,10 +36,14 @@ class App extends Component {
       <div className="container">
         <div className="box-container">
           <div className="box red">
-            <span className="box-label">left: 0 top: 0</span>
+            <span className="box-label">
+              left: {this.state.redBox.left} top: {this.state.redBox.top}
+            </span>
           </div>
           <div className="box blue">
-            <span className="box-label">left: 0 top: 0</span>
+            <span className="box-label">
+              left: {this.state.blueBox.left} top: {this.state.blueBox.top}
+            </span>
           </div>
         </div>
         <button className="button shuffle" onClick={this.shuffle}>
